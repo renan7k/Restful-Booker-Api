efba239709d48589f615f5bd0ffe06d192848482
Renan Dias <renan7k@gmail.com>
2024-12-21 16:34:54 -0300

    add tests create Token

--- cypress/e2e/createToken.cy.js
+++ cypress/e2e/createToken.cy.js
@@ -1,8 +1,5 @@
 /// <reference types="cypress" />
 describe('Funcionalidade de criar token de acesso - API', () => {
-
-  beforeEach(() => {
-  })
   it('Verificar criação de token com sucesso', () => {
     const credentials = {
       "username" : "admin",
@@ -16,7 +13,72 @@ describe('Funcionalidade de criar token de acesso - API', () => {
 
         expect(response.body.token).is.not.empty
         expect(response.body.token).is.not.null
-        Cypress.env('ACCESS_TOKEN', response.body.token)
+
+        //Armazenando token gerado. Pendente de como enviar para uma variável no cypress env, para utilizar em outros testes
+        const token = response.body.token
+        cy.log(token)
+        Cypress.env('ACCESS_TOKEN', token)
       })
+  })
+
+  it('Verificar mensagem de retorno ao tentar criar token com username inválido', () => {
+    const credentials = {
+      "username" : "teste12",
+      "password" : "password123"
+    }
+
+    cy.postCreateToken(credentials).then((response) => {
+      // Ponto de melhoria - Rota retorna 200, mas talvez o melhor seria 401 - Unauthorized
+      // expect(response.status).to.eq(200) 
+      expect(response.body).to.have.property('reason')
+
+      expect(response.body.reason).is.equal('Bad credentials')
+    })
+
+  })
+
+  it('Verificar mensagem de retorno ao tentar criar token com senha inválida', () => {
+    const credentials = {
+      "username" : "admin",
+      "password" : "senha123"
+    }
+
+    cy.postCreateToken(credentials).then((response) => {
+      // Ponto de melhoria - Rota retorna 200, mas talvez o melhor seria 401 - Unauthorized
+      // expect(response.status).to.eq(200) 
+      expect(response.body).to.have.property('reason')
+
+      expect(response.body.reason).is.equal('Bad credentials')
+    })
+  })
+
+  it('Verificar mensagem de retorno ao tentar criar token sem preenchimento do username', () => {
+    const credentials = {
+      "username" : "",
+      "password" : "senha123"
+    }
+
+    cy.postCreateToken(credentials).then((response) => {
+      // Ponto de melhoria - Rota retorna 200, mas talvez o melhor seria 401 - Unauthorized
+      // expect(response.status).to.eq(200) 
+      expect(response.body).to.have.property('reason')
+
+      expect(response.body.reason).is.equal('Bad credentials')
+    })
+  })
+
+  it.only('Verificar mensagem de retorno ao tentar criar token sem preenchimento do password', () => {
+    const credentials = {
+      "username" : "admin",
+      "password" : ""
+    }
+
+    cy.postCreateToken(credentials).then((response) => {
+      // Ponto de melhoria - Rota retorna 200, mas talvez o melhor seria 401 - Unauthorized
+      // expect(response.status).to.eq(200) 
+      expect(response.body).to.have.property('reason')
+
+      expect(response.body.reason).is.equal('Bad credentials')
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
 Cypress.Commands.add('postCreateToken', (credentials) => {
-    cy.request({
+    cy.api({
         method: 'POST',
         url: 'https://restful-booker.herokuapp.com/auth',
         body: { 
@@ -15,3 +15,22 @@ Cypress.Commands.add('postCreateToken', (credentials) => {
         //PENDENTE COMO ENVIAR O TOKEN PARA O CYPRESS.ENV
     })
 })
+
+//Este segundo comando customizado, é específico para os testes de outras rotas que necessitam de autenticação
+Cypress.Commands.add('createToken', (credentials) => {
+    cy.api({
+        method: 'POST',
+        url: 'https://restful-booker.herokuapp.com/auth',
+        body: { 
+            "username": credentials.username,
+            "password": credentials.password
+        },
+        headers: {'Content-Type': 'application/json'}
+    }).then((response) => {
+        // Armazena o token no Cypress
+        const token = response.body.token;
+        
+        // Salva o token em um alias para usar em outros testes
+        cy.wrap(token).as('authToken');
+    });
+});
